2efd753fb03833eb77957504f96c99c397efea41
VnuchkovAY <sasha_vnuchkov@mail.ru>
2021-05-19 20:54:25 +0300

    Initial Commit

---
+++ Vnuchkov_AY_HW2.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Vnuchkov_AY_HW2.1
+//
+//  Created by Внучков Саша on 19.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
